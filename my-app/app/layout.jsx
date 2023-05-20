62154146213ab6b5bd9c2743c73e3e78d49589e5
BELLRECONzTech <122868815+BELLRECONzTech@users.noreply.github.com>
2023-05-20 18:25:52 +0700

    Navbar 80%

---
+++ my-app/app/layout.jsx
@@ -0,0 +1,27 @@
+import '@styles/global.css'
+import Nav from '@components/Nav'
+import Provider from '@components/Provider'
+
+export const metadeta = {
+    title : "Promptopia is Awesome",
+    description : "Discover & Share AI prompts",
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang = "en">
+        <body>
+            <div className='main'>
+                <div  className = 'gradient'/>
+            </div>
+
+            <main className='app'>
+                <Nav/>
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout
